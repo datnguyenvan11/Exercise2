539786487f536ad94e84baf58a253a0411b09501
John Smith <example@email.com>
2019-07-22 15:20:42 +0700

    nopbai2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter a String");
+           var String = Console.ReadLine();
+           Console.WriteLine("Enter a word to search");
+           var search = Console.ReadLine();
+           var count = Regex.Matches(String, search).Count;
+           Console.WriteLine($"Word found { count } times in the string");        }
+    }
+}
